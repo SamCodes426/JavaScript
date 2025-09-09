5c00b17687b92c11abf4b5f26dbfddd71f184bb4
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-09 19:15:25 +0530

    Video5: learn about datatypes

---
+++ Video5.js
@@ -0,0 +1,36 @@
+// ============================= DATATYPES ===================================
+
+"use strict"; // treat all JS code as newer JS version
+
+
+// alert( 3+3 ) // we are using node JS not browser
+
+let name = "samar";               // string
+
+let age = 18;                     // number 2^53 is the limit 
+
+let isLoggedIn = false;           // boolean -> true/false
+
+let state;                        // undefined (value has not been assigned)
+
+let temperature = null;           // null (standalone value)
+ 
+                                  // symbol (unique)
+
+                                  // object 
+ 
+
+// =============================== TYPEOF ===================================
+
+
+console.log(typeof"samar");      // string 
+
+console.log(typeof 3);           // number
+
+console.log(typeof isLoggedIn);  // boolean
+
+console.log(typeof null);        // object
+
+console.log(typeof undefined);   // undefined
+
+// ==========================================================================
