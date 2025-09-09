935cedfdff09c5f4b271570b3ef1063d09b04674
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-09 18:34:39 +0530

    video1: what is javascript

---
+++ Video1.js
@@ -0,0 +1,6 @@
+/* ================================== JAVASCRIPT =================================
+
+ JavaScript is a high-level, interpreted programming language primarily used for
+  creating dynamic and interactive content on websites and web applications
+
+=================================================================================*/
