a331efee0b085e1c80e9f4190657f70b897628da
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-09 18:35:10 +0530

    video2: enviroment and execution

---
+++ Video2.js
@@ -0,0 +1,12 @@
+// ================================ ENVIRONMENT AND EXECUTION =====================================
+
+                                console.log("Samarpit Maurya") 
+                    
+                    // it is used to print the output to the console 
+
+                        
+                                    //  node filename.js  
+                            
+                        //this is command is used to run the file 
+
+// ================================================================================================
