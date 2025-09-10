972c3ec4269fad036310ba0bc7fdbf0813529f18
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-11 01:53:52 +0530

    Video8: comparison of datatypes

---
+++ Video8.js
@@ -0,0 +1,27 @@
+console.log("2" == 2); //output: true
+console.log("2" > 1);  //output: true             here string is getting typecased into int for comparison
+
+
+console.log(null > 0); //output: false
+console.log(null == 0);//output: false
+console.log(null >= 0);//output: true
+
+console.log(undefined > 0);//output: false
+console.log(undefined >= 0);//output: false
+console.log(undefined <= 0);//output: false
+console.log(undefined == 0);//output: false
+
+//  === (triple equals to is called as strict check)
+
+// it check for the datatype if it match the datatype or not 
+
+console.log("1" === 1);//output: false
+
+
+
+
+
+
+
+
+
