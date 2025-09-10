05ee31b45c8e09508a637c22314555a8f4f71eef
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-11 01:39:41 +0530

    Video7: string to number conversion complexity

---
+++ Video7.js
@@ -0,0 +1,19 @@
+console.log("1"+2);         // output: 12
+console.log(1+"2");         // output: 12
+console.log("1"+2+2);       // output: 122
+console.log(1+2+"2");       // output: 32
+
+console.log(+true);         //output: 1
+console.log(+"");           //output: 0
+
+
+let a = 10;
+b=a++;                          // a value is stored in b after printing (post increment)
+console.log("a "+a,"b "+b);
+
+
+
+
+
+
+
