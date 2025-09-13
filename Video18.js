271f5e17832504ed7a25883b8433bbd93a85d297
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-14 02:18:08 +0530

    video18: object de-structure and JASON api

---
+++ Video18.js
@@ -0,0 +1,21 @@
+// ===================== OBJECT DESTRUCTURE ======================
+
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseinstructor : "Sam"
+}
+
+const {courseinstructor: instructor} = course           //Object de-structure
+
+console.log(instructor);
+
+
+// =============================== JSON API ===================================
+
+// {
+//     "name": "samar",
+//     "coursename": "js in hindi"
+//     "price": "free"
+// }
