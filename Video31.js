7983352a0e4073752a293df4421065d1549cd063
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-19 00:10:45 +0530

    Video31: DOM introduction

---
+++ Video31.js
@@ -0,0 +1,17 @@
+//                 WINDOW
+//                    |
+//                    |
+//                    |
+//                 DOCUMENT
+//                    |
+//                    |
+//                    |
+//                   HTML
+                  
+// INSIDE HTML
+
+// 1. HEAD ---> Title ---> text node
+//         ---> meta  ---> attribute
+
+// 2. BODY ---> div ---> h1 ---> attribute / text node
+//                  ---> p ---> text node
