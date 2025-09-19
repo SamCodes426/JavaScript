77dc3f77f0da1c831867cfba5fde8643b563de44
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-20 02:15:58 +0530

    Video37: async js

---
+++ Video37.js
@@ -0,0 +1,25 @@
+// =========================== ASync ========================
+
+// javascript (default)
+//     >synchronus         //ek ke baad ek execute hota h 
+//     >single threaded    
+
+
+
+
+
+// execution useContext
+//     >execute one line of code at a time     (each operation waits for the last one to complete before executing)
+
+
+
+
+
+
+// BLOCKING CODE V/S NON BLOCKING CODE
+
+// Blocking code --> block the flow of program ---> read file sync
+
+// Non Blocking code --> does not block execution --> read file async
+
+    
