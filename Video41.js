2c2a047f0d455195b533c47a6c075b3c6ac599fa
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-20 23:51:15 +0530

    Video41: learn fetch

---
+++ Video41.js
@@ -0,0 +1,16 @@
+// ==================================  FETCH ===================================================
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+   return response.json()
+    
+})
+.then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
